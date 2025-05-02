df0ab046dc61ef24a814b0a05ab02385fb8c18c0
kevinbrianndungu <kevinbrianndungu@gmail.com>
2025-05-02 12:47:27 +0300

    collector

--- test_data_collector.py
+++ test_data_collector.py
@@ -1,5 +1,9 @@
+import json
 from utils.data_collector import collect_all_data
 
 if __name__ == "__main__":
+    # Collect all data
     data = collect_all_data()
+
+    # Print the data in a readable JSON format
     print(json.dumps(data, indent=4))
